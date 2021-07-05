eea5b9d23783b0077379e9ea7bae344f9f7fb4cb
unknown <zb.zhu@sky-mobi.com>
2021-07-05 18:23:28 +0800

    fifth

--- src/pages/dataready/index.jsx
+++ src/pages/dataready/index.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
-import { Upload, message,Progress } from 'antd';
-import { CloudUploadOutlined } from '@ant-design/icons';
+import { Upload, message } from 'antd';
+import { CloudUploadOutlined,FolderOpenOutlined } from '@ant-design/icons';
 
 const { Dragger } = Upload;
+
+
 export default class DataReady extends Component {
 
     state={
-        percent: 0
+        percent: 0,
+        processWidth: 0,
+        fileName: '',
+        isShowProgress: false
     }
 
     getUpload = ()=>{
@@ -14,24 +19,36 @@ export default class DataReady extends Component {
         const pro = {
             name: 'file',
             multiple: true,
-            action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-            
-            // onUploading(e){
-            //     const xhr = new XMLHttpRequest();
-            //     console.log('我是进度条')
-            //     xhr.upload.onprogress =(e)=>{
-            //         let percent = Math.floor((e.loaded/e.total)*100)
-            //         console.log('我是进度条'+percent)
-            //         _this.setState({
-            //             percent: percent
-            //         })    
-            //     }
-            // },
-
+            showUploadList: false,
+            action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',                     
+            beforeUpload(info) {               
+                const isCSV = info.type === 'application/vnd.ms-excel';
+                if (!isCSV) {
+                    message.error('上传文件的格式必须为csv！')
+                    return false;
+                } 
+                // else {
+                //     _this.setState({   
+                //         isShowProgress : true,                       
+                //     })   
+                // }
+            },          
             onChange(info) {
               const { status } = info.file;
-              if (status !== 'uploading') {
-                  console.log(info.file.size/1000/556.89)
+              const event = info.event
+              if(event) {
+                    let percent = ((event.loaded / event.total)* 100).toFixed(2)
+                    let processWidth = (event.loaded / event.total)*100
+                    const fileName = info.file.name
+                    _this.setState({  
+                        isShowProgress: true,
+                        fileName,                       
+                        percent,
+                        processWidth,                       
+                    })   
+              }
+              if (status !== 'uploading') {            
+                //   console.log(e)
                 // const xhr = new XMLHttpRequest();
                 // console.log('我是进度条'+info)
                 // xhr.upload.onprogress =(e)=>{
@@ -41,7 +58,7 @@ export default class DataReady extends Component {
                 //         percent: percent
                 //     })    
                 // }
-                console.log(info.file, info.fileList+'我在上传中');
+                console.log(info.file,info.file.name, info.fileList+'我在上传中');
                 
               }
               if (status === 'done') {
@@ -54,47 +71,57 @@ export default class DataReady extends Component {
                     console.log('lalalalalala', e.dataTransfer.files);                  
                     // let percent = Math.floor((e.loaded/e.total)*100);
                      console.log(e)
-                    // console.log('进度条在这'+percent)
-                    // console.log(e.loaded)
-                    // console.log(e.total)
-                    // console.log(percent)
-                    // let upload = this.state.upload;
-                    // upload.progress.loaded = e.loaded;
-                    // upload.progress.total = e.total;
-                    // upload.progress.percentage = percent;
-                    // _this.setState({
-                    //     percent: percent
-                    // })    
             },
           };
 
           return pro
     }
+    
+    componentDidMount(){
+        this.getUpload()
+    }
 
- 
-  
     render() {
             const {...pro} = this.getUpload()
-            console.log(this.getUpload())
+            const {percent,isShowProgress,processWidth,fileName} = this.state
         return (
             <div style={{width:'70%',height:'579.39px',margin:'auto',marginTop:'120px'}}>
-                <Dragger {...pro}>
-                <p className="ant-upload-drag-icon" style={{ height:'60px',margin:0}} >
-                <CloudUploadOutlined style={{fontSize:'60px',color:'#3569d6'}}/>
-                </p>
-                <br/>
-                <p style={{fontSize:'20px',color:'#4675da'}}>上传源数据</p>
-                <br/>
-                <p className="ant-upload-text" style={{color:'#adaeb3'}}>数据格式为csv，建议大小不超过300Mb</p>
-                <br/>
-                <p className="ant-upload-hint">
-                    'BILL_DRGCODE','SYS_DRGCODE','RESPONSE_JSON'必须存在
-                </p>
-            </Dragger>
-                <div>
-                    <Progress percent={this.state.percent} />
-                </div>
+                {
+                    isShowProgress === false ? (
+                        <Dragger {...pro} >
+                            <p className="ant-upload-drag-icon" style={{ height:'60px',margin:0}} >
+                            <CloudUploadOutlined style={{fontSize:'60px',color:'#3569d6'}}/>
+                            </p>
+                            <br/>
+                            <p style={{fontSize:'20px',color:'#4675da'}}>上传源数据</p>
+                            <br/>
+                            <p className="ant-upload-text" style={{color:'#adaeb3'}}>数据格式为csv，建议大小不超过300Mb</p>
+                            <br/>
+                            <p className="ant-upload-hint">
+                                'BILL_DRGCODE','SYS_DRGCODE','RESPONSE_JSON'必须存在
+                            </p>
+                    </Dragger>
+                    )  
+                    :(
+                        <>                  
+                            <div className='succPogress'>
+                                <span style={{position:'absolute'}}><FolderOpenOutlined style={{fontSize:'20px',marginLeft:'20px'}}/>&nbsp;&nbsp;&nbsp;{fileName}</span>
+                                <div style={{width:`${processWidth}%`,lineHeight:'78px',height:'78px',backgroundColor:'#eaeffa',borderRadius:'15px'}}>{percent+'%'}</div>                               
+                            </div>
+                            <span>ssssssssssss</span>
+                            {/* <Progress {...pro} strokeColor={'#eaeffa'}
+                            className= 'ant-progress-bgm' 
+                            percent={percent}
+                            trailColor ={'white'}
+                            > </Progress> */}
+                        </>
+                        
+                    )
+                }
+                    
+                
             </div>
+            
             
         )
     }
